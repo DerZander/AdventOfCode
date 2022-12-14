dce6c031dad310f347ae2d9e427bfae1059d6be3
DerZander <timo.zander@outlook.com>
2022-12-14 17:43:35 +0100

    feat: solved day four

--- 2022/04_day/solution.py
+++ 2022/04_day/solution.py
@@ -1,45 +1,73 @@
+import os
+
 from advent_handler import AdventHandler
 
 
-class SectionCleanup(AdventHandler):
-    def __init__(self, day, year, is_testing):
+class Solution(AdventHandler):
+    def __init__(self, day=0, year=0, is_testing=False):
         super().__init__(day, year, is_testing)
-        self.section_pairs = self.get_section_pairs()
+        self.pairs = self.get_pairs()
 
-    def get_section_pairs(self):
-        section_pairs = []
-        sections_list = open(self.file_path, "r").readlines()
-        for sl in sections_list:
-            section = {}
-            section_pair = sl.split("\n")[0].split(",")
-            section["one"] = {"range": section_pair[0], "numbers": []}
-            section["two"] = {"range": section_pair[1], "numbers": []}
-            for s_char in ["one", "two"]:
-                range_one = section[s_char]["range"].split("-")[0]
-                range_two = section[s_char]["range"].split("-")[1]
-                for n in range(int(range_one), int(range_two) + 1):
-                    section[s_char]["numbers"].append(n)
-
-            section_pairs.append(section)
-        return section_pairs
+    def get_pairs(self):
+        pairs = []
+        lines = open(self.file_path)
+        for line in lines:
+            line = line.split("\n")[0].split(",")
+            pair_one = line[0]
+            range_one = []
+            for i in range(int(pair_one.split("-")[0]), int(pair_one.split("-")[1]) + 1):
+                range_one.append(i)
+            pair_two = line[1]
+            range_two = []
+            for i in range(int(pair_two.split("-")[0]), int(pair_two.split("-")[1]) + 1):
+                range_two.append(i)
+            sections = {"pair_one": pair_one, "pair_two": pair_two, "range_one": range_one, "range_two": range_two}
+            pairs.append(sections)
+        return pairs
 
     def answer_one(self):
-        print(self.section_pairs)
         counter = 0
-        for section_pair in self.section_pairs:
-            for section in section_pair["one"]["numbers"]:
-                if int(section_pair["two"]["range"].split("-")[1]) > section > int(
-                        section_pair["two"]["range"].split("-")[0]):
+        for pair in self.pairs:
+            counter_updated = False
+            pair_range = len(pair["range_one"])
+            pair_counter = 0
+            for p in pair["range_one"]:
+                if p in pair["range_two"]:
+                    pair_counter += 1
+            if pair_counter == pair_range:
+                counter += 1
+                counter_updated = True
+            pair_range = len(pair["range_two"])
+            pair_counter = 0
+            for p in pair["range_two"]:
+                if p in pair["range_one"]:
+                    pair_counter += 1
+            if pair_counter == pair_range:
+                if not counter_updated:
                     counter += 1
-                    break
         print(counter)
+        self.result_1 = counter
 
     def answer_two(self):
-        pass
+        ol_counter = 0
+        for pair in self.pairs:
+            counter = 0
+            for p in pair["range_one"]:
+                if p in pair["range_two"]:
+                    counter += 1
+            for p in pair["range_two"]:
+                if p in pair["range_one"]:
+                    counter += 1
+            if counter > 0:
+                ol_counter += 1
+
+        print(ol_counter)
+        self.result_2 = ol_counter
 
 
 if __name__ == "__main__":
     print(f"Start of: {__file__}")
-    sc = SectionCleanup(4, 2022, False)
-    sc.answer_one()
-    sc.answer_two()
+    s = Solution()
+    s.answer_one()
+    s.answer_two()
+    s.set_answers()
